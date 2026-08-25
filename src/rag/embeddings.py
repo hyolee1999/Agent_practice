9564833e4bce2d2216ba0a9558b6f325ba01069e
Khoa <bndk2108@gmail.com>
2026-08-24 21:46:40 -0400

    Fix QdrantVectorStore connection

--- src/rag/embeddings.py
+++ src/rag/embeddings.py
@@ -3,11 +3,14 @@ from langchain_qdrant import FastEmbedSparse
 from langchain_ollama import OllamaEmbeddings
 
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
+
+OLLAMA_BASE_URL = os.getenv("OLLAMA_BASE_URL")
 
 sparse_embeddings = FastEmbedSparse(model_name="Qdrant/bm25")
 
 # dense_embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
 
-dense_embeddings = OllamaEmbeddings(model="embeddinggemma")
+dense_embeddings = OllamaEmbeddings(model="embeddinggemma", base_url = OLLAMA_BASE_URL)
--- src/rag/vector_store.py
+++ src/rag/vector_store.py
@@ -45,23 +45,26 @@ def retriever_init(collection_name) -> QdrantVectorStore:
         # and a sparse vector named "sparse" (as created below). Explicitly
         # provide these names so that QdrantVectorStore validates correctly.
 
-        # qdrant = QdrantVectorStore.from_existing_collection(
-        #     collection_name=collection_name,
-        #     embedding=dense_embeddings,
-        #     sparse_embedding=sparse_embeddings,
-        #     retrieval_mode=RetrievalMode.HYBRID,
-        #     vector_name="dense",
-        #     sparse_vector_name="sparse"        )
-
-        qdrant = QdrantVectorStore(
-            client=client,
+        qdrant = QdrantVectorStore.from_existing_collection(
             collection_name=collection_name,
             embedding=dense_embeddings,
             sparse_embedding=sparse_embeddings,
             retrieval_mode=RetrievalMode.HYBRID,
             vector_name="dense",
             sparse_vector_name="sparse",
-        )
+            validate_collection = False,
+            url=QDRANT_URL,
+            api_key=QDRANT_API_KEY)
+
+        # qdrant = QdrantVectorStore(
+        #     client=client,
+        #     collection_name=collection_name,
+        #     embedding=dense_embeddings,
+        #     sparse_embedding=sparse_embeddings,
+        #     retrieval_mode=RetrievalMode.HYBRID,
+        #     vector_name="dense",
+        #     sparse_vector_name="sparse",
+        # )
 
     else:
         # Determine the correct size for dense vectors.
